4d749ed151d18f62516b36027e155a2e0390d208
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-01-09 16:36:43 +0200

    feat: created all app tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,113 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import {Container} from "@material-ui/core";
+import WarRoom from "./pages/war-room";
+import Shuttles from "./pages/shuttles";
+import Supervisors from "./pages/supervisors";
+import Riders from "./pages/riders";
+import Statistics from "./pages/statistics";
+import Settings from "./pages/settings";
 
-function App() {
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`nav-tabpanel-${index}`}
+      aria-labelledby={`nav-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
   );
 }
 
-export default App;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `nav-tab-${index}`,
+    'aria-controls': `nav-tabpanel-${index}`,
+  };
+}
+
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      onClick={event => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function NavTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Container fixed>
+        <AppBar position="static">
+          <Tabs
+            variant="fullWidth"
+            value={value}
+            onChange={handleChange}
+            aria-label="Shuttles System"
+          >
+            <LinkTab label="War Room" href="/" {...a11yProps(0)} />
+            <LinkTab label="Shuttles" href="/shuttles" {...a11yProps(1)} />
+            <LinkTab label="Supervisors" href="/supervisors" {...a11yProps(2)} />
+            <LinkTab label="Riders" href="/riders" {...a11yProps(3)} />
+            <LinkTab label="Statistics" href="/statistics" {...a11yProps(4)} />
+            <LinkTab label="Settings" href="/settings" {...a11yProps(5)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          <WarRoom/>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <Shuttles/>
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <Supervisors/>
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          <Riders/>
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          <Statistics/>
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          <Settings/>
+        </TabPanel>
+      </Container>
+    </div>
+  );
+}

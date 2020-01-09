f07bacb4d0818dcae51f8fc5f8971caf01f8e239
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-01-09 09:42:04 +0200

    feat: init react app

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/pickmeup`
 
 app.use(bodyParser.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)

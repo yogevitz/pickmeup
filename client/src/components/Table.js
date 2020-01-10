de7ff03030505cb17e40d651619e087e18c80ba9
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-01-10 12:58:37 +0200

    feat: updated temporary hard coded data for shuttles

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -20,22 +20,57 @@ import Remove from '@material-ui/icons/Remove';
 export default function Table() {
   const [state, setState] = React.useState({
     columns: [
+      { title: 'ID', field: 'id', type: 'numeric' },
       { title: 'Name', field: 'name' },
-      { title: 'Surname', field: 'surname' },
-      { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+      { title: 'Contact Name', field: 'contactName' },
+      { title: 'Contact Phone', field: 'contactPhone' },
       {
-        title: 'Birth Place',
-        field: 'birthCity',
-        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        title: 'Destination',
+        field: 'destination',
       },
     ],
     data: [
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
       {
-        name: 'Zerya Betül',
-        surname: 'Baran',
-        birthYear: 2017,
-        birthCity: 34,
+        id: 1,
+        name: 'Beer Sheva Yellow Bus',
+        contactName: 'Yosi Cohen',
+        contactPhone: '0528108333',
+        destination: 'Beer Sheva',
+      },
+      {
+        id: 2,
+        name: 'Beit Kama Mini Bus',
+        contactName: 'Ben Levi',
+        contactPhone: '0523712321',
+        destination: 'Beit Kama',
+      },
+      {
+        id: 3,
+        name: 'BK Large Bus',
+        contactName: 'Goni Levin Haimi',
+        contactPhone: '0548332918',
+        destination: 'Beit Kama',
+      },
+      {
+        id: 4,
+        name: 'Rahat 10',
+        contactName: 'Hassan Abudugush',
+        contactPhone: '0546478899',
+        destination: 'Rahat',
+      },
+      {
+        id: 5,
+        name: 'Lehavim School Bus',
+        contactName: 'Yaniv Gabot',
+        contactPhone: '0544718732',
+        destination: 'Lehavim',
+      },
+      {
+        id: 6,
+        name: 'Mishmar 2',
+        contactName: 'Dani Duenias',
+        contactPhone: '0541112827',
+        destination: 'Mishmar Hanegev',
       },
     ],
   });

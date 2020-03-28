895aec02512b9b41845c33404daf05653289bf58
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-03-29 02:15:58 +0300

    feat: added shuttle autocomplete for war room

--- client/src/pages/war-room/war-room.js
+++ client/src/pages/war-room/war-room.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import Table from "../../components/Table";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { getAllShuttles } from "../../proxy";
 
 class WarRoom extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      shuttles: [],
+    };
+  }
+
+  async componentWillMount() {
+    const shuttles = await getAllShuttles();
+    this.setState({ shuttles: shuttles });
   }
 
   render() {
+    const { shuttles } = this.state;
     return (
       <div>
-        War Room
+        <Autocomplete
+          id="shuttle-select"
+          options={shuttles}
+          getOptionLabel={(option) => option.name}
+          style={{ width: 300 }}
+          renderInput={(params) =>
+            <TextField {...params} label="Shuttle" variant="outlined" />}
+        />
       </div>
     );
   }

56a0a030656563f9b4d8e9bd5c252ed353e727bf
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-04-28 16:48:08 +0300

    feat: created generic info alert component

---
+++ client/src/components/InfoAlert.js
@@ -0,0 +1,28 @@
+import React from "react";
+import MuiAlert from "@material-ui/lab/Alert/Alert";
+import Snackbar from "@material-ui/core/Snackbar/Snackbar";
+
+export const INFO_ALERT_TEXT = {
+  add: 'Successfully Added :)',
+  delete: 'Successfully Deleted!',
+  update: 'Successfully Updated :)',
+};
+
+export const INFO_ALERT_SEVERITY = {
+  add: 'success',
+  delete: 'warning',
+  update: 'info',
+};
+
+export class InfoAlert extends React.Component {
+  render() {
+    const { isOpen, onClose, severity, text } = this.props;
+    return (
+      <Snackbar open={isOpen} autoHideDuration={3000} onClose={onClose}>
+        <MuiAlert elevation={6} variant="filled" onClose={onClose} severity={severity}>
+          {text}
+        </MuiAlert>
+      </Snackbar>
+    );
+  }
+}

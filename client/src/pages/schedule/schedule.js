0a479652064d25795c062b2896603834eeddad2c
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-05-11 00:39:10 +0300

    feat: added supervisor picker for schedule page

--- client/src/pages/schedule/schedule.js
+++ client/src/pages/schedule/schedule.js
@@ -22,70 +22,121 @@ import { getAllShuttles, getAllSupervisors } from "../../proxy";
 
 const SHIFT_KEY = 16;
 
-const appointments = [
-  {
-    title: 'Yogev Shlomovitz',
-    supervisor: 0,
-    startDate: new Date(2020, 4, 10),
-    endDate: new Date(2020, 4, 11),
-    id: 0,
-  },
-  {
-    title: 'Yogev Shlomovitz',
-    supervisor: 0,
-    startDate: new Date(2020, 4, 11),
-    endDate: new Date(2020, 4, 12),
-    id: 1,
-  },
-  {
-    title: 'Idan Shani',
-    supervisor: 1,
-    startDate: new Date(2020, 4, 12),
-    endDate: new Date(2020, 4, 13),
-    id: 2,
-  },
-  {
-    title: 'Hadar Nataf',
-    supervisor: 2,
-    startDate: new Date(2020, 4, 13),
-    endDate: new Date(2020, 4, 14),
-    id: 3,
-  },
-  {
-    title: 'Hadar Nataf',
-    supervisor: 2,
-    startDate: new Date(2020, 4, 14),
-    endDate: new Date(2020, 4, 15),
-    id: 4,
-  },
-  {
-    title: 'Tomer Gabay',
-    supervisor: 3,
-    startDate: new Date(2020, 4, 15),
-    endDate: new Date(2020, 4, 16),
-    id: 5,
-  },
-];
+const appointments = {
+  '1': [
+    {
+      title: 'Yogev Shlomovitz',
+      supervisor: '311530018',
+      startDate: new Date(2020, 4, 10),
+      endDate: new Date(2020, 4, 11),
+      id: 0,
+    },
+    // {
+    //   title: 'Yogev Shlomovitz',
+    //   supervisor: '311530018',
+    //   startDate: new Date(2020, 4, 11),
+    //   endDate: new Date(2020, 4, 12),
+    //   id: 1,
+    // },
+    // {
+    //   title: 'Idan Shani',
+    //   supervisor: '315662485',
+    //   startDate: new Date(2020, 4, 12),
+    //   endDate: new Date(2020, 4, 13),
+    //   id: 2,
+    // },
+    // {
+    //   title: 'Bar Perez',
+    //   supervisor: '302548452',
+    //   startDate: new Date(2020, 4, 13),
+    //   endDate: new Date(2020, 4, 14),
+    //   id: 3,
+    // },
+    // {
+    //   title: 'Bar Perez',
+    //   supervisor: '302548452',
+    //   startDate: new Date(2020, 4, 14),
+    //   endDate: new Date(2020, 4, 15),
+    //   id: 4,
+    // },
+    // {
+    //   title: 'Guy Kaplan',
+    //   supervisor: '309887272',
+    //   startDate: new Date(2020, 4, 15),
+    //   endDate: new Date(2020, 4, 16),
+    //   id: 5,
+    // },
+  ],
+  '2': [
+    {
+      title: 'Idan Shani',
+      supervisor: '315662485',
+      startDate: new Date(2020, 4, 10),
+      endDate: new Date(2020, 4, 11),
+      id: 0,
+    },
+    {
+      title: 'Bar Perez',
+      supervisor: '302548452',
+      startDate: new Date(2020, 4, 11),
+      endDate: new Date(2020, 4, 12),
+      id: 1,
+    },
+    {
+      title: 'Guy Kaplan',
+      supervisor: '309887272',
+      startDate: new Date(2020, 4, 12),
+      endDate: new Date(2020, 4, 13),
+      id: 2,
+    },
+    {
+      title: 'Bar Perez',
+      supervisor: '302548452',
+      startDate: new Date(2020, 4, 13),
+      endDate: new Date(2020, 4, 14),
+      id: 3,
+    },
+    {
+      title: 'Yogev Shlomovitz',
+      supervisor: '311530018',
+      startDate: new Date(2020, 4, 14),
+      endDate: new Date(2020, 4, 15),
+      id: 4,
+    },
+    {
+      title: 'Guy Kaplan',
+      supervisor: '309887272',
+      startDate: new Date(2020, 4, 15),
+      endDate: new Date(2020, 4, 16),
+      id: 5,
+    },
+  ],
+};
 
-const supervisors = [
-  {
-    text: 'Yogev Shlomovitz',
-    id: 0,
-    color: '#7E57C2',
-  }, {
-    text: 'Idan Shani',
-    id: 1,
-    color: '#FF7043',
-  }, {
-    text: 'Hadar Nataf',
-    id: 2,
-    color: '#54c239',
-  }, {
-    text: 'Tomer Gabay',
-    id: 3,
-    color: '#6397ff',
-  }
-];
+const BasicLayout = ({ onFieldChange, appointmentData, ...restProps }) => {
+  const onSupervisorChange = (event, newValue) => {
+    onFieldChange({
+      supervisor: newValue ? newValue.id : undefined,
+      title: newValue ? newValue.text : undefined,
+    });
+  };
+
+  return (
+    <Grid container justify="center" spacing={3} style={{ marginTop: '5px', textAlign: 'center' }}>
+      <Grid item xs={5}>
+        <Autocomplete
+          id="supervisor-select"
+          options={restProps.resources[0].instances}
+          getOptionLabel={option => option.text}
+          onChange={onSupervisorChange}
+          value={restProps.appointmentResources ? restProps.appointmentResources[0] : undefined}
+          renderInput={(params) =>
+            <TextField {...params} label="Supervisor" variant="outlined" />}
+        />
+      </Grid>
+    </Grid>
+  );
+};
 
 class Schedule extends React.Component {
   constructor(props) {
@@ -96,15 +147,8 @@ class Schedule extends React.Component {
       selectedShuttle: undefined,
       selectedDate: new Date(),
       selectedSupervisor: undefined,
-      data: appointments,
-      resources: [
-        {
-          fieldName: "supervisor",
-          title: "Supervisor",
-          instances: supervisors,
-          allowMultiple: false
-        }
-      ],
+      data: [],
+      resources: [],
       isShiftPressed: false,
     };
   }
@@ -139,12 +183,35 @@ class Schedule extends React.Component {
     });
   };
 
+  mapSupervisors = supervisor => ({
+    ...supervisor,
+    id: supervisor.supervisorID,
+    text: supervisor.name,
+    color: this.getRandomColor(),
+  });
+
+  getRandomColor = () => {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
   async componentDidMount() {
     window.addEventListener('keydown', this.onKeyDown);
     window.addEventListener('keyup', this.onKeyUp);
     const shuttles = await getAllShuttles();
-    const supervisors = await getAllSupervisors();
-    this.setState({ shuttles, supervisors });
+    const supervisorsData = await getAllSupervisors();
+    const supervisors = supervisorsData.map(this.mapSupervisors);
+    const resources = [{
+      title: "Supervisor",
+      fieldName: "supervisor",
+      instances: supervisors,
+      allowMultiple: false
+    }];
+    this.setState({ shuttles, supervisors, resources });
   }
 
   componentWillUnmount() {
@@ -164,27 +231,34 @@ class Schedule extends React.Component {
     }
   };
 
+  onChangeSelectedShuttle = (event, newValue) => {
+    this.setState({
+      selectedShuttle: newValue,
+      data: newValue && newValue['shuttleID']
+        ? appointments[newValue['shuttleID']]
+        : [],
+    });
+  };
+
   render() {
-    const { shuttles } = this.state;
+    const { shuttles, selectedShuttle } = this.state;
     return (
       <div>
         <MuiPickersUtilsProvider utils={MomentUtils}>
           <Grid container justify="center" spacing={3} style={{ marginTop: '5px', textAlign: 'center' }}>
-            <Grid item xs={3}>
+            <Grid item xs={5}>
               <Autocomplete
                 id="shuttle-select"
                 options={shuttles}
                 getOptionLabel={(option) => option.name}
-                onChange={(event, newValue) => {
-                  this.setState({ selectedShuttle: newValue});
-                }}
+                onChange={this.onChangeSelectedShuttle}
                 renderInput={(params) =>
                   <TextField {...params} label="Shuttle" variant="outlined" />}
               />
             </Grid>
           </Grid>
           <Grid container justify="center" spacing={3} style={{ marginTop: '15px', textAlign: 'center' }}>
-            <Grid item xs={10}>
+            {selectedShuttle && <Grid item xs={10}>
               <Paper>
                 <Scheduler data={this.state.data} >
                   <ViewState />
@@ -200,12 +274,14 @@ class Schedule extends React.Component {
                     showOpenButton
                     showDeleteButton
                   />
-                  <AppointmentForm />
+                  <AppointmentForm
+                    basicLayoutComponent={BasicLayout}
+                  />
                   <Resources data={this.state.resources} mainResourceName="supervisor" />
                   <DragDropProvider allowResize={() => false}/>
                 </Scheduler>
               </Paper>
-            </Grid>
+            </Grid>}
           </Grid>
         </MuiPickersUtilsProvider>
       </div>

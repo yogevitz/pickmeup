7e9b5cb5094eb22e8914bc080e057d92f8cd9b92
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-05-10 19:14:06 +0300

    feat: initialized supervisors schedule page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
--- client/src/pages/schedule/schedule.js
+++ client/src/pages/schedule/schedule.js
@@ -1,21 +1,76 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
+import Paper from '@material-ui/core/Paper';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Button from '@material-ui/core/Button';
-import MomentUtils from '@date-io/moment';
+import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-} from '@material-ui/pickers';
+  Scheduler,
+  Resources,
+  MonthView,
+  Toolbar,
+  DateNavigator,
+  Appointments,
+  TodayButton,
+  AppointmentTooltip,
+  AppointmentForm,
+  EditRecurrenceMenu,
+  DragDropProvider,
+} from '@devexpress/dx-react-scheduler-material-ui';
+import MomentUtils from '@date-io/moment';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { getAllShuttles, getAllSupervisors } from "../../proxy";
 
-const directions = [ 'Morning', 'Afternoon' ];
+const appointments = [
+  {
+    title: 'Yogev Shlomovitz',
+    supervisor: 0,
+    startDate: new Date(2020, 4, 3),
+    endDate: new Date(2020, 4, 7),
+    id: 0,
+  },
+  {
+    title: 'Idan Shani',
+    supervisor: 1,
+    startDate: new Date(2020, 4, 7),
+    endDate: new Date(2020, 4, 12),
+    id: 1,
+  },
+  {
+    title: 'Hadar Nataf',
+    supervisor: 2,
+    startDate: new Date(2020, 4, 12),
+    endDate: new Date(2020, 4, 21),
+    id: 2,
+  },
+  {
+    title: 'Tomer Gabay',
+    supervisor: 3,
+    startDate: new Date(2020, 4, 21),
+    endDate: new Date(2020, 4, 31),
+    id: 3,
+  },
+];
+
+const supervisors = [
+  {
+    text: 'Yogev Shlomovitz',
+    id: 0,
+    color: '#7E57C2',
+  }, {
+    text: 'Idan Shani',
+    id: 1,
+    color: '#FF7043',
+  }, {
+    text: 'Hadar Nataf',
+    id: 2,
+    color: '#54c239',
+  }, {
+    text: 'Tomer Gabay',
+    id: 3,
+    color: '#6397ff',
+  }
+];
 
 class Schedule extends React.Component {
   constructor(props) {
@@ -25,28 +80,53 @@ class Schedule extends React.Component {
       supervisors: [],
       selectedShuttle: undefined,
       selectedDate: new Date(),
-      selectedDirection: undefined,
       selectedSupervisor: undefined,
+      data: appointments,
+      resources: [
+        {
+          fieldName: "supervisor",
+          title: "Supervisor",
+          instances: supervisors,
+          allowMultiple: false
+        }
+      ]
     };
   }
 
-  handlePickSupervisor = event => {
-    this.setState({ supervisor: event.target.value })
+  commitChanges = ({ added, changed, deleted }) => {
+    this.setState(state => {
+      let { data } = state;
+      if (added) {
+        const startingAddedId =
+          data.length > 0 ? data[data.length - 1].id + 1 : 0;
+        data = [...data, { id: startingAddedId, ...added }];
+      }
+      if (changed) {
+        data = data.map(appointment =>
+          changed[appointment.id]
+            ? { ...appointment, ...changed[appointment.id] }
+            : appointment
+        );
+      }
+      if (deleted !== undefined) {
+        data = data.filter(appointment => appointment.id !== deleted);
+      }
+      return { data };
+    });
   };
 
   async componentDidMount() {
     const shuttles = await getAllShuttles();
     const supervisors = await getAllSupervisors();
-    this.setState({ shuttles: shuttles, supervisors: supervisors });
+    this.setState({ shuttles, supervisors });
   }
 
   render() {
-    const { shuttles, supervisors, selectedShuttle, selectedDate, selectedDirection, selectedSupervisor } = this.state;
-    const showAssignPanel = !!selectedShuttle && !!selectedDate && !!selectedDirection;
+    const { shuttles } = this.state;
     return (
       <div>
         <MuiPickersUtilsProvider utils={MomentUtils}>
-          <Grid container justify="center" spacing={3} style={{ marginTop: '6px', textAlign: 'center' }}>
+          <Grid container justify="center" spacing={3} style={{ marginTop: '5px', textAlign: 'center' }}>
             <Grid item xs={3}>
               <Autocomplete
                 id="shuttle-select"
@@ -59,72 +139,30 @@ class Schedule extends React.Component {
                   <TextField {...params} label="Shuttle" variant="outlined" />}
               />
             </Grid>
-            <Grid item xs={3}>
-              <KeyboardDatePicker
-                margin="normal"
-                id="date-picker-dialog"
-                label="Date"
-                autoOk
-                rifmFormatter={val=> val.replace(/[^\.\ \,\[a-zA-Z0-9_]*$]+/gi, '')}
-                refuse={/[^\.\ \,\[a-zA-Z0-9_]*$]+/gi}
-                format={"MMM DD, YYYY"}
-                value={selectedDate}
-                onChange={(event, newValue) => {
-                  this.setState({ selectedDate: newValue});
-                }}
-                KeyboardButtonProps={{
-                  'aria-label': 'change date',
-                }}
-              />
-            </Grid>
-            <Grid item xs={3}>
-              <Autocomplete
-                id="direction-select"
-                options={directions}
-                getOptionLabel={(option) => option}
-                onChange={(event, newValue) => {
-                  this.setState({ selectedDirection: newValue});
-                }}
-                renderInput={(params) =>
-                  <TextField {...params} label="Direction" variant="outlined" />}
-              />
-            </Grid>
           </Grid>
-          <Divider style={{ marginTop: '18px' }} />
-          <Grid container justify="center" spacing={3} style={{ marginTop: '6px', textAlign: 'center' }}>
-            <Grid item>
-              {showAssignPanel ?
-                (<div>
-                  <Grid item xs={12}>
-                    Choose supervisor:
-                  </Grid>
-                  <Grid item xs={12}>
-                    <FormControl component="fieldset">
-                      <RadioGroup
-                        aria-label="gender"
-                        name="gender1"
-                        value={selectedSupervisor}
-                        onChange={() => {}}
-                      >
-                        {supervisors.map(value => (
-                            <FormControlLabel
-                              key={`supervisor-${value.supervisorID}`}
-                              value={value.name}
-                              control={<Radio />} label={value.name}
-                            />))}
-                      </RadioGroup>
-                    </FormControl>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Button variant="contained" color="primary" style={{marginTop: '12px'}}>
-                      Save
-                    </Button>
-                  </Grid>
-                </div>)
-                :
-                (<div>
-                  Please choose shuttle, date and direction
-                </div>)}
+          <Grid container justify="center" spacing={3} style={{ marginTop: '15px', textAlign: 'center' }}>
+            <Grid item xs={10}>
+              <Paper>
+                <Scheduler data={this.state.data} >
+                  <ViewState />
+                  <EditingState onCommitChanges={this.commitChanges} />
+                  <EditRecurrenceMenu />
+                  <IntegratedEditing />
+                  <MonthView />
+                  <Toolbar />
+                  <DateNavigator />
+                  <TodayButton />
+                  <Appointments />
+                  <AppointmentTooltip
+                    showCloseButton
+                    showOpenButton
+                    showDeleteButton
+                  />
+                  <AppointmentForm />
+                  <Resources data={this.state.resources} mainResourceName="supervisor" />
+                  <DragDropProvider allowResize={() => false}/>
+                </Scheduler>
+              </Paper>
             </Grid>
           </Grid>
         </MuiPickersUtilsProvider>
